ce65d86985579ef46a55e3c3476bb8bfa242e86b
Stian <shu034@student.uib.no>
2018-01-24 15:47:38 +0100

    Reverted

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import asyncio
-import random
 
 import config
 
@@ -16,7 +15,7 @@ async def on_ready():
     print('ID: ' + bot.user.id)
     print('------')
 
-'''
+
 @bot.command(pass_context=True, alias="v")
 @commands.has_role("Greeter")
 async def verify(ctx, *, member: discord.Member):
@@ -27,21 +26,5 @@ async def verify(ctx, *, member: discord.Member):
         await bot.say("Gave {} member!".format(member.name))
     except Exception:
         await bot.say("Couldn't make {} member...".format(member.name))
-'''
 
-
-@bot.command()
-async def roll(dice: str):
-    """Roll a dice in NdN format."""
-    try:
-        rolls = int(dice.split("d")[0])
-        faces = int(dice.split("d")[1])
-    except Exception:
-        await bot.say("Must be in NdN format")
-    result = []
-    for i in range(rolls):
-        result.append(str(random.randint(1, faces)))
-    await bot.say("You rolled: {}".format(", ".join(result)))
-
-
-bot.run(config.USER_TOKEN if config.SELF_BOT else config.BOT_TOKEN)
+bot.run(config.TOKEN)
